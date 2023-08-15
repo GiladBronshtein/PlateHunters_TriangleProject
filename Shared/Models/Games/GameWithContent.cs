31d0c307fd066a1fe25215268ff3ac3f1ff0838f
Gilad Bronshtein <job.gilad@gmail.com>
2023-08-15 15:43:50 +0300

    Commit DB and minor changes

--- Shared/Models/Games/GameWithContent.cs
+++ Shared/Models/Games/GameWithContent.cs
@@ -10,9 +10,14 @@ namespace TriangleProject.Shared.Models.Games
 	public class GameWithContent
 	{
 		public int ID { get; set; }
-		public string GameCode { get; set; }
+        public int UserID { get; set; } //maybe we don't need that
+        public string GameCode { get; set; }
 		public string GameName { get; set; }
-		public List<GameAnswers> Answers { get; set; }
-		public GameQuestions Questions { get; set; }
-	}
+        public string QuestionDescription { get; set; }
+        public string GameEndMessage { get; set; }
+        public string CorrectCategoryName { get; set; }
+        public string WrongCategoryName { get; set; }
+        public List<GameAnswers> Answers { get; set; }
+        public bool IsPublished { get; set; }
+    }
 }
