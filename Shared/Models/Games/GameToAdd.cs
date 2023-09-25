d5d2d846411af640057c056505ab9ab05d9b051d
Gilad Bronshtein <job.gilad@gmail.com>
2023-09-25 14:52:42 +0300

    Edit answer - Phase 1 - Show data on inputs

--- Shared/Models/Games/GameToAdd.cs
+++ Shared/Models/Games/GameToAdd.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace TriangleProject.Shared.Models.Games
 {
     public class GameToAdd
     {
+        [Required(ErrorMessage = "יש להזין שם משחק")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "שם המשחק חייב להכיל 2 תווים לפחות")]
         public string GameName { get; set; }
     }
 }
